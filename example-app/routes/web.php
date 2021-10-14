be9b25ad9c0daff6f85342d774c94decb4cd55f4
gurshant <gurshant53@gmail.com>
2021-10-14 13:56:31 -0700

    resouce controller (crud)

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,56 +30,46 @@ Route::get('/contact', [HomeController::class, 'contact'])
 Route::get('/about', AboutController::class)
     ->name('home.about');
 
-$posts = [
-    1 => [
-        'title' => 'Intro to Laravel',
-        'content' => 'This is a short intro to Laravel',
-        'is_new' => true,
-    ],
-    2 => [
-        'title' => 'Intro to PHP',
-        'content' => 'This is a short intro to PHP',
-        'is_new' => false,
-    ],
-];
-
 // get specific post
 // Verification for a num happens in RouteServiceProvider
-Route::get('/posts/{id}', function ($id) use ($posts) {
+// Route::get('/posts/{id}', function ($id) use ($posts) {
 
-    // Abort if post is not found
-    abort_if(!isset($posts[$id]), 404);
-    return view('posts.show', ['post' => $posts[$id]]);
-})->name('posts.show');
+//     // Abort if post is not found
+//     abort_if(!isset($posts[$id]), 404);
+//     return view('posts.show', ['post' => $posts[$id]]);
+// })->name('posts.show');
 
-Route::get('/posts', function () use ($posts) {
-    // dd(request()->all());
-    dd((int) request()->input('page', 1));
-    return view('posts.index', ['posts' => $posts]);
-})->name('posts.index');
+// Route::get('/posts', function () use ($posts) {
+//     // dd(request()->all());
+//     dd((int) request()->input('page', 1));
+//     return view('posts.index', ['posts' => $posts]);
+// })->name('posts.index');
+
+// Could also use except instead of only
+Route::resource('posts', PostsController::class)->only(['index', 'show']);
 
 // get with optional param
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
     return 'Posts from ' . $daysAgo . ' days ago';
 })->name('posts.recent.index')->middleware('auth');
 
-// Group function fun
-Route::prefix('/fun')->name('fun.')->group(function () use ($posts) {
+// // Group function fun
+// Route::prefix('/fun')->name('fun.')->group(function () use ($posts) {
 
-    Route::get('/responses', function () use ($posts) {
-        return response($posts, 201)
-            ->header('Content-Type', 'application/json')
-            ->cookie('MY_COOKIE', 'P J', 3600);
-    })->name('responses');
+//     Route::get('/responses', function () use ($posts) {
+//         return response($posts, 201)
+//             ->header('Content-Type', 'application/json')
+//             ->cookie('MY_COOKIE', 'P J', 3600);
+//     })->name('responses');
 
-// redirect to posts.show[1]
-    Route::get('/redirect', function () {
-        return redirect()->route('posts.show', ['id' => 1]);
-    })->name('redirect');
+// // redirect to posts.show[1]
+//     Route::get('/redirect', function () {
+//         return redirect()->route('posts.show', ['id' => 1]);
+//     })->name('redirect');
 
-// go back
-    Route::get('/back', function () {
-        return back();
-    })->name('back');
+// // go back
+//     Route::get('/back', function () {
+//         return back();
+//     })->name('back');
 
-});
+// });
